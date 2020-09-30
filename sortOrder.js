4499f7a7728c5079f863f95d7b3cf0db8f402f2b
github username <github email address>
2020-09-29 21:30:57 -0600

    JS array labs

---
+++ sortOrder.js
@@ -0,0 +1,12 @@
+const firstValue = process.argv[2];
+const secondValue = process.argv[3];
+
+if ('Z' > 'A'){
+    console.log(-1)
+// } else if (treeLifeStatus === 1) {
+//     console.log("flowering")
+}else if ('A'  > 'Z') {
+    console.log(1)
+} else {
+    console.log(0)
+}
