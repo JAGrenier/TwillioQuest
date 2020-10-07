1ab64f9b269ecd1c42f42dd68157995524b90142
github username <github email address>
2020-10-06 19:51:16 -0600

    -solve for sortOrder

---
+++ freightScanner.js
@@ -0,0 +1,15 @@
+function scan(freightItems) {
+    let contrabandCount = 0;
+
+
+    for(contrabandCount = 0; contrabandCount === 'contraband'; contrabandCount++)
+
+    
+
+    return contrabandCount;
+}
+
+// The following lines of code are not required for the solution, but can be
+// used by you to test your solution.
+const numItems = scan(['dog', 'contraband', 'cat', 'zippers', 'contraband']);
+  console.log('Number of "contraband": ' + numItems); // should be 2
--- sortOrder.js
+++ sortOrder.js
@@ -1,12 +1,10 @@
 const firstValue = process.argv[2];
 const secondValue = process.argv[3];
 
-if ('Z' > 'A'){
-    console.log(-1)
-// } else if (treeLifeStatus === 1) {
-//     console.log("flowering")
-}else if ('A'  > 'Z') {
+if (firstValue.toLowerCase() > secondValue.toLowerCase()){
     console.log(1)
+}else if (secondValue.toLowerCase()  > firstValue.toLowerCase()) {
+    console.log(-1)
 } else {
     console.log(0)
 }
